b94d8598b3822f1f9fe0a8ab070819b0564ed856
ruiqiang <rxqjack@126.com>
2016-09-12 15:35:12 +0800

    数据库变更is_db => p_db

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=isdb',
+    'dsn' => 'mysql:host=localhost;dbname=p_db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
